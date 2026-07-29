68266249e67d623cc437970df03d8e4b2dee1511
Anton Wijs <A.J.Wijs@tue.nl>
2026-07-29 18:15:10 +0200

    Exercises 4 and 5

---
+++ 4-reduction_fast/reduction_fast.cu
@@ -0,0 +1,170 @@
+#include <stdio.h>
+
+#define block_size_x 256
+#define num_blocks 1024
+
+
+extern "C" {
+    void start_timer();
+    void stop_timer(float *time);
+    __global__ void reduce_kernel(float *out_array, float *in_array, int n);
+}
+
+//A naive summation in C
+float sum_floats(float *in_array, int n) {
+    float sum = 0.0;
+    for (int i=0; i<n; i++) {
+        sum += in_array[i];
+    }
+    return sum;
+}
+
+//Kahan summation to avoid floating-point precision errors
+float sum_floats_kahan(float *in_array, int n) {
+    float sum = 0.0;
+    float c = 0.0;
+    for (int i=0; i<n; i++) {
+        float v = in_array[i] - c;
+        float t = sum + v;
+        c = (t - sum) - v;
+        sum = t;
+    }
+    return sum;
+}
+
+//CUDA kernel for parallel reduction
+__global__ void reduce_kernel(float *out_array, float *in_array, int n) {
+
+    int ti = threadIdx.x;
+    int x = blockIdx.x * block_size_x + threadIdx.x;
+    int step_size = gridDim.x * block_size_x;
+    float sum = 0.0f;
+
+    //Cooperatively (with all threads in all thread blocks) iterate over input array
+    for (int i=x; i<n; i+=step_size) {
+        sum += in_array[i];
+    }
+
+    //At the point we have reduced the number of values to be summed from n to
+    //the total number of threads in all thread blocks combined
+
+    //The goal is now to reduce the values within each thread block to a single
+    //value per thread block. For this we will need shared memory
+
+    //Declare shared memory array, how much shared memory do we need?
+    //__shared__ float ...;
+
+    //Make every thread store its thread-local sum to the array in shared memory
+    //... = sum;
+    
+    //Now let's call syncthreads() to make sure all threads have finished
+    //storing their local sums to shared memory
+    __syncthreads();
+
+    //Now this interesting looking loop will do the following:
+    //It iterates over the block_size_x with the following values for s:
+    //If block_size_x is 256, 's' will be powers of 2 from 128, 64, 32, down to 1.
+    //these decreasing offsets can be used to reduce the number
+    //of values within the thread block in only a few steps.
+    #pragma unroll
+    for (unsigned int s=block_size_x/2; s>0; s/=2) {
+
+        //You are to write the code inside this loop such that
+        //threads will add the sums of other threads that are 's' away.
+        //Do this iteratively such that together the threads compute the
+        //sum of all thread-local sums 
+
+        //Use shared memory to access the values of other threads
+        //and store the new value in shared memory to be used in the next round
+        //Be careful that values that should be read are
+        //not overwritten before they are read
+        //Make sure to call __syncthreads() when needed
+    }
+
+    //Write back one value per thread block
+    if (ti == 0) {
+        //out_array[blockIdx.x] = ;  //store the per-thread block reduced value to global memory
+    }
+}
+
+
+int main() {
+
+    int n = (int)5e7; //problem size
+    float time;
+    cudaError_t err;
+
+    //Allocate arrays and fill them
+    float *in_array = (float *) malloc(n * sizeof(float));
+    float *out_array = (float *) malloc(num_blocks * sizeof(float));
+    for (int i=0; i<n; i++) {
+        in_array[i] = (rand() % 10000) / 100000.0;
+    }
+    memset(out_array, 0, num_blocks * sizeof(float));
+
+    //Measure the CPU function
+    start_timer();
+    float sum = sum_floats(in_array, n);
+    stop_timer(&time);
+    printf("sum_floats took %.3f ms\n", time);
+
+    //Allocate GPU memory
+    float *d_in; float *d_out;
+    err = cudaMalloc((void **)&d_in, n*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMalloc: %s\n", cudaGetErrorString( err ));
+    err = cudaMalloc((void **)&d_out, num_blocks*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMalloc: %s\n", cudaGetErrorString( err ));
+
+    //Copy the input data to the GPU
+    err = cudaMemcpy(d_in, in_array, n*sizeof(float), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemcpy host to device: %s\n", cudaGetErrorString( err ));
+
+    //Zero the output array
+    err = cudaMemset(d_out, 0, num_blocks*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemset: %s\n", cudaGetErrorString( err ));
+
+    //Setup the grid and thread blocks
+    dim3 grid(num_blocks, 1);
+    dim3 grid2(1, 1);
+    dim3 threads(block_size_x, 1, 1);
+
+    //Measure the GPU function
+    cudaDeviceSynchronize();
+    start_timer();
+    reduce_kernel<<<grid, threads>>>(d_out, d_in, n);
+    reduce_kernel<<<grid2, threads>>>(d_out, d_out, num_blocks); //call the kernel again with only 1 thread block
+    cudaDeviceSynchronize();
+    stop_timer(&time);
+    printf("reduce_kernel took %.3f ms\n", time);
+
+    //Check to see if all went well
+    err = cudaGetLastError();
+    if (err != cudaSuccess) fprintf(stderr, "Error during kernel launch: %s\n", cudaGetErrorString( err ));
+
+    //Copy the result back to host memory
+    err = cudaMemcpy(out_array, d_out, 1*sizeof(float), cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemcpy device to host: %s\n", cudaGetErrorString( err ));
+
+    //Compute a reliable reference answer on the host
+    float sum2 = sum_floats_kahan(in_array, n);
+
+    //Check the result
+    float diff = abs(*out_array - sum2);
+    printf("cpu: %f, corrected: %f\n", sum, sum2);
+    printf("gpu: %f\n", *out_array);
+
+    if (diff < 1.0) {
+        printf("TEST PASSED!\n");
+    } else {
+        printf("TEST FAILED!\n");
+    }
+
+    //Clean up
+    cudaFree(d_in);
+    cudaFree(d_out);
+    free(in_array);
+    free(out_array);
+
+    return 0;
+}
+
---
+++ 5-matrix_sum/matrix_sum.cu
@@ -0,0 +1,161 @@
+#include <stdio.h>
+
+extern "C" {
+    void start_timer();
+    void stop_timer(float *time);
+    __global__ void vec_sum_kernel(float *c, float *a, int nrows, int ncols);
+}
+
+int compare_arrays(float *c, float *d, int n);
+
+void matrix_sum(float *c, float *a, int nrows, int ncols) {
+    for (int i=0; i<nrows; i++) {
+    	for (int j=0; j<ncols; j++) {
+    		c[i] += a[i*ncols+j];
+    	}
+    }
+}
+
+__global__ void matrix_sum_kernel(float *c, float *a, int nrows, int ncols) {
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (x < nrows) {
+        //To do: change for-loop to correctly fetch the row elements
+    	for (int i = 0; i < ncols; i++) {
+    		c[x] += a[x*ncols+i];
+    	}
+    }
+}
+
+
+int main() {
+
+    int nrows = 1e8; //Number of rows in matrices
+    int ncols = 4; //Number of columns in matrices
+    float time;
+    cudaError_t err;
+
+    //Allocate arrays and fill them
+    float *a = (float *) malloc(nrows * ncols * sizeof(float));
+    float *c = (float *) malloc(nrows * sizeof(float));
+    float *d = (float *) malloc(nrows * sizeof(float));
+    for (int i=0; i<(nrows * ncols); i++) {
+        a[i] = 1.0 / rand();
+    }
+    for (int i=0; i<nrows; i++) {
+    	c[i] = 0.0;
+        d[i] = 0.0;
+    }
+
+    float *e = (float *) malloc(nrows*ncols*sizeof(float));
+
+    //Measure the CPU function
+    start_timer();
+    matrix_sum(c, a, nrows, ncols);
+    stop_timer(&time);
+    printf("matrix_add took %.3f ms\n", time);
+
+    //To do: rearrange data in matrix a to coalesce memory accesses
+    //You can use matrix e for this
+    float temp = 0;
+    for (int i=0; i<nrows; i++)
+    {
+    	for (int j=0; j<ncols; j++)
+    	{
+		// ...
+    	}
+    }
+    a = e;
+
+    //Allocate GPU memory
+    float *d_a; float *d_c;
+    err = cudaMalloc((void **)&d_a, nrows*ncols*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMalloc d_a: %s\n", cudaGetErrorString( err ));
+    err = cudaMalloc((void **)&d_c, nrows*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMalloc d_c: %s\n", cudaGetErrorString( err ));
+
+    //Copy the input data to the GPU
+    err = cudaMemcpy(d_a, a, nrows*ncols*sizeof(float), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemcpy host to device a: %s\n", cudaGetErrorString( err ));
+
+    //Zero the output array
+    err = cudaMemset(d_c, 0, nrows*sizeof(float));
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemset c: %s\n", cudaGetErrorString( err ));
+
+    //Setup the grid and thread blocks
+    int block_size = 1024;                          	//thread block size
+    int nblocks = int(ceilf(nrows/(float)block_size));  //nrows divided by thread block size rounded up
+    dim3 grid(nblocks, 1);
+    dim3 threads(block_size, 1, 1);
+
+    //Measure the GPU function
+    cudaDeviceSynchronize();
+    start_timer();
+    matrix_sum_kernel<<<grid, threads>>>(d_c, d_a, nrows, ncols);
+    cudaDeviceSynchronize();
+    stop_timer(&time);
+    printf("matrix_sum_kernel took %.3f ms\n", time);
+
+    //Check to see if all went well
+    err = cudaGetLastError();
+    if (err != cudaSuccess) fprintf(stderr, "Error during kernel launch matrix_sum_kernel: %s\n", cudaGetErrorString( err ));
+
+    //Copy the result back to host memory
+    err = cudaMemcpy(d, d_c, nrows*sizeof(float), cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) fprintf(stderr, "Error in cudaMemcpy device to host c: %s\n", cudaGetErrorString( err ));
+
+    //Check the result
+    int errors = compare_arrays(c, d, nrows);
+    if (errors > 0) {
+        printf("TEST FAILED!\n");
+    } else {
+        printf("TEST PASSED!\n");
+    }
+
+
+    //Clean up
+    cudaFree(d_a);
+    cudaFree(d_c);
+    free(a);
+    free(c);
+    free(d);
+
+    return 0;
+}
+
+
+
+int compare_arrays(float *a1, float *a2, int n) {
+    int errors = 0;
+    int print = 0;
+
+    for (int i=0; i<n; i++) {
+
+        if (isnan(a1[i]) || isnan(a2[i])) {
+            errors++;
+            if (print < 10) {
+                print++;
+                fprintf(stderr, "Error NaN detected at i=%d,\t a1= %10.7e \t a2= \t %10.7e\n",i,a1[i],a2[i]);
+            }
+        }
+
+        unsigned int int_a1 = *(unsigned int *)(a1+i);
+        unsigned int int_a2 = *(unsigned int *)(a2+i);
+        unsigned int dist = (unsigned int)0;
+        if (int_a1 > int_a2) {
+            dist = int_a1 - int_a2;
+        } else {
+            dist = int_a2 - int_a1;
+        }
+        if (dist > 0) {
+            errors++;
+            if (print < 10) {
+                print++;
+                fprintf(stderr, "Error detected at i=%d, \t a1= \t %10.7e \t a2= \t %10.7e \t ulp_dist=\t %u\n",i,a1[i],a2[i],dist);
+            }
+        }
+
+    }
+
+    return errors;
+}
