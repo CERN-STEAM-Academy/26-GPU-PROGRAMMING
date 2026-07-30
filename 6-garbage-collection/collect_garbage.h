a10e70a88a05af47a6a15a764cb08979cdf7bf56
Jan Heemstra <heemstrajan@gmail.com>
2026-07-30 10:10:58 +0200

    More description for the exercise

--- 6-garbage-collection/collect_garbage.h
+++ 6-garbage-collection/collect_garbage.h
@@ -21,10 +21,29 @@
   * layer to see which nodes are orphaned and can be removed.
   */
 void garbage_collection(auto policy, auto& v, auto& parent_v) {
-	// Compiler computes types
+	// Templating logic to deduce type of vector (host/device)
 	using Vec = std::remove_reference_t<decltype(v)>;
+
+	// Templating logic to construct the right type of vector (host/device) with our own type (uint32_t)
+	// StencilVec is thrust::device_vector<uint32_t> or thrust::host_vector<uint32_t>
 	using StencilVec = rebind_vector_t<Vec, uint32_t>; // Why use uint32_t and not bool?
+
 	
 	// Generate list of referenced nodes
+	// This initializes a thrust::(host|device)_vector called stencil with v.size() elements, all initialized to `1`
 	StencilVec stencil(v.size(), 1);
+
+
+	// TODO: implement garbage collection
+
+	/*
+	Example:
+	Adding 1 to each element of stencil
+	thrust::transform(
+		policy, // This is either thrust::host or thrust::device
+		stencil.begin(), stencil.end(), // Input list
+		stencil.begin(),	// Output list. Can be same as input list, but must not overlap with offset.
+		[]__host__ __device__(uint32_t in) { return in + 1; }
+	);
+	*/ 
 };
